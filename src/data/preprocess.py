69481a4611e2f52dec6175ceeff6884133bc27f9
JuanTobon25 <86813137+JuanTobon25@users.noreply.github.com>
2025-05-03 12:59:02 -0500

    Update preprocess.py

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import wandb
 
-wandb.login(key="f505fc5086438e923a5d468c900a748275a8ff7c", relogin=True)
+wandb.login(key="41eaef54f8dd6ac2d3b892988e6ab0f5c40331f8", relogin=True)
 # --- Argumentos ---
 parser = argparse.ArgumentParser()
 parser.add_argument('--IdExecution', type=str, default="testing console", help='ID of the execution')

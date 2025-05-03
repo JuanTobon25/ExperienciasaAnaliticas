6e8ade59da34f562d85f720d880484176ebcaa1b
JuanTobon25 <86813137+JuanTobon25@users.noreply.github.com>
2025-05-03 12:58:45 -0500

    Update load.py

--- src/data/load.py
+++ src/data/load.py
@@ -19,7 +19,7 @@ def parse_args():
 
 args = parse_args()
 # --- Configuración de WandB ---
-wandb.login(key="f505fc5086438e923a5d468c900a748275a8ff7c", relogin=True)
+wandb.login(key="41eaef54f8dd6ac2d3b892988e6ab0f5c40331f8", relogin=True)
 
 # --- Cargar el dataset Iris y dividirlo ---
 def load_and_split(train_size=0.8):
